f9c60665440c8743deaf709112dfb8fa6d4bd320
manojkmgit <39760825+manojkmgit@users.noreply.github.com>
2021-11-28 22:48:53 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Learn Docker here</title>
+</head>
+<body>
+    <h1>Learn Docker Now With Us and PHP</h1>
+    <p><?php echo "hostname is:".gethostname(); ?></p>
+</body>
